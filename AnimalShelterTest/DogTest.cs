add2afa1b569e6e476ddce4441c200c120fb6dd1
GeertBoer <geert.boer555@gmail.com>
2016-09-29 20:52:06 +0200

    DogTest gemerged

---
+++ AnimalShelterTest/DogTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnimalShelter;
+
+namespace AnimalShelterTest
+{
+    [TestClass]
+    public class DogTest
+    {
+        SimpleDate date = new SimpleDate(12, 12, 1999);
+        [TestMethod]
+        public void LastWalkDateTest()
+        {
+            Dog d = new Dog(38, date, "hundje", date);
+            d.LastWalkDate = date;
+            Assert.AreEqual(d.LastWalkDate, date);
+        }
+
+        [TestMethod]
+        public void CheckPriceWhenChipNrIsLowerThan50000()
+        {
+            Dog d = new Dog(1, date, "hundje", date);
+            Assert.AreEqual(200.00m, d.Price);
+        }
+
+        [TestMethod]
+        public void checkPriceWhenChipNrIs50000()
+        {
+            Dog d = new Dog(50000, date, "hundje", date);
+            Assert.AreEqual(350.00m, d.Price);
+        }
+
+        [TestMethod]
+        public void checkPriceWhenChipNrIsZero()
+        {
+            Dog d = new Dog(0, date, "hundje", date);
+            Assert.AreEqual(200.00m, d.Price);
+        }
+    }
+}
