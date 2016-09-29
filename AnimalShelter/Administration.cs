942c6774db327f45a1ea729e8753328c878d2b86
Geert <geert.boer555@gmail.com>
2016-09-29 20:47:59 +0200

    """"Initial"""""" commit

---
+++ AnimalShelter/Administration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnimalShelter
+{
+    class Administration
+    {
+        private List<Animal> animals; 
+
+        public Administration()
+        {
+            animals = new List<Animal>();
+        }
+
+        public bool Add(Animal animal)
+        {         
+            if(animal != null)
+            {
+                animals.Add(animal);
+                return true;
+            }
+            return false;  
+        }
+
+        public bool RemoveAnimal(int chipRegistrationNumber)
+        {
+            foreach (Animal a in animals)
+            {
+                if (a.ChipRegistrationNumber == chipRegistrationNumber)
+                {
+                    animals.Remove(a);                    
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Animal FindAnimal(int chipRegistrationNumber)
+        {
+            foreach (Animal a in animals)
+            {
+                if(a.ChipRegistrationNumber == chipRegistrationNumber)
+                {
+                    return a;
+                }
+            }
+            return null;
+        }
+
+        public bool checkChipNr(int nr)
+        {
+            foreach (Animal a in animals)
+            {
+                if (a.ChipRegistrationNumber == nr)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
